454c5439fa662cddb79a0f09be88535c1edcc4a8
Sergen1993 <14488@coderacademy.edu.au>
2023-05-12 18:08:21 +1000

    products page added

---
+++ python/SergenTasdivar_T1A3/products.py
@@ -0,0 +1,7 @@
+products = [
+    {'name': 'Corteiz x Nike Air Max 95 SP', 'price': 383},
+    {'name': 'New Balance x Aimé Leon Dore 860v2', 'price': 450},
+    {'name': 'Converse x ADER ERROR Chuck 70', 'price': 214},
+    {'name': 'KITH x Clarks x adidas Samba', 'price': 925},
+    {'name': "Nike Air Jordan 1 Retro Low OG 'Year of the Rabbit'", 'price': 800},
+]
