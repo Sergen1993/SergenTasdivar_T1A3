41d2ae0eed32224f523378dcf6d125e8b1ca1060
Sergen1993 <14488@coderacademy.edu.au>
2023-05-12 18:14:11 +1000

    main.py was updated to import products.py

--- python/SergenTasdivar_T1A3/main.py
+++ python/SergenTasdivar_T1A3/main.py
@@ -1,14 +1,7 @@
 import sys
 from tabulate import tabulate
 from termcolor import colored
-
-products = [
-    {'name': 'Corteiz x Nike Air Max 95 SP', 'price': 383},
-    {'name': 'New Balance x Aimé Leon Dore 860v2', 'price': 450},
-    {'name': 'Converse x ADER ERROR Chuck 70', 'price': 214},
-    {'name': 'KITH x Clarks x adidas Samba', 'price': 925},
-    {'name': "Nike Air Jordan 1 Retro Low OG 'Year of the Rabbit'", 'price': 800},
-]
+from products import products
 
 cart = []
 
